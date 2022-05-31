2836fcc6321f189bd6034281ffc2a41d586ddba3
Arunabh Das <arunabhdas@gmail.com>
2022-05-31 19:24:08 -0400

    Added input field

--- vivaldi/src/App.jsx
+++ vivaldi/src/App.jsx
@@ -1,8 +1,13 @@
-
+import {createSignal} from "solid-js";
 function App() {
+  const [keyword, setKeyword] = createSignal("Anachron")
   return (
-    <div>
+    <div class="container">
       <h1>Welcome to Vivaldi</h1>
+      <input onKeyUp={(e) =>
+      setKeyword(e.currentTarget.value)}
+      type="text" />
+      <p> { keyword }</p>
     </div>
   );
 }
